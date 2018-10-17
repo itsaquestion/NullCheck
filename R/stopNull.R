ff9149e8aeeb2cfbac00c11bc3ba4242a0187720
itsaquestion <itsaquestion@gmail.com>
2018-10-18 02:21:06 +0800

    rename checkNull() to stopNull()

--- R/stopNull.R
+++ R/stopNull.R
@@ -1,4 +1,4 @@
-#' checkNull
+#' stopNull
 #'
 #' Check caller's args and stop if there are any NULLs. 
 #' @param except args' name vector that should not to be checked
@@ -9,7 +9,7 @@
 #' @importFrom dplyr filter
 #' @examples
 #' add = function(a, b, c) {
-#' 	checkNull()
+#' 	stopNull()
 #' 	a + b + c
 #' }
 #' 
@@ -18,12 +18,12 @@
 #' add(1, 2, NULL)
 #' 
 #' add2 = function(a, b, c) {
-#' 	checkNull(except = "c")
+#' 	stopNull(except = "c")
 #' 	a + b
 #' }
 #' 
 #' add2(1, 2, NULL)
-checkNull = function(except = NULL) {
+stopNull = function(except = NULL) {
 
 	obj_list = parent.frame(n = 1)
 
@@ -32,24 +32,23 @@ checkNull = function(except = NULL) {
 	is_null_obj = sapply(obj_list, is.null)
 	obj_names = names(is_null_obj)
 
-	df = data.frame(obj_names, is_null_obj, stringsAsFactors = F) 
-	
+	df = data.frame(obj_names, is_null_obj, stringsAsFactors = F)
+
 	if (!is.null(except)) {
 		df = dplyr::filter(df, !(obj_names %in% except))
 	}
 
-	df = dplyr::filter(df,is_null_obj)
+	df = dplyr::filter(df, is_null_obj)
 
 	if (nrow(df) == 0) { return() }
 
-	caller = tail(as.character(as.list(sys.call(-1))[[1]]),1)
+	caller = tail(as.character(as.list(sys.call(-1))[[1]]), 1)
 
-	null_obj_names = paste(df$obj_names,"= NULL", collapse = ", ") 
+	null_obj_names = paste(df$obj_names, "= NULL", collapse = ", ")
 
 	error_message = "Null args in function: \r\t{caller}({null_obj_names})"
 
 	stop(glue::glue(error_message), call. = FALSE)
-	
-}
 
+}
 
